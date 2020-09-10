4dc7358db7644cffddc9795d431e55ebc37d91c6
Kevin Cuadros <kev040798@gmail.com>
2020-09-10 11:27:54 -0400

    Create interests test

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -17,4 +17,15 @@ class PatronTest < Minitest::Test
     assert_equal 20, patron_1.spending_money
     assert_equal [], patron_1.interests
   end
+
+  def test_it_has_interests
+    patron_1 = Patron.new("Bob", 20)
+    exhibit1 = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    exhibit2 = Exhibit.new({name: "Dead Sea Scrolls", cost: 0})
+
+    patron_1.add_interests(exhibit2.name)
+    patron_1.add_interests(exhibit1.name)
+
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], patron_1.interests
+  end
 end
