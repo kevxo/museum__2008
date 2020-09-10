a9a1e0156ed42c8b5ee7e7b1491577cc43deed2e
Kevin Cuadros <kev040798@gmail.com>
2020-09-10 11:59:55 -0400

    Add method  for recommend exhibits

--- lib/museum.rb
+++ lib/museum.rb
@@ -8,4 +8,16 @@ class Museum
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
+
+  def recommend_exhibits(patron)
+    exhibit_interest = []
+    patron.interests.each do |interest|
+      @exhibits.each do |exhibit|
+        if interest == exhibit.name
+          exhibit_interest << exhibit
+        end
+      end
+    end
+    exhibit_interest
+  end
 end
